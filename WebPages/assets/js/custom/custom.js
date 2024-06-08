c849a7717d0d089671aa6668c814590a641b625e
Gruff James <gruffjameswork@gmail.com>
2024-06-08 20:11:59 +0100

    Add API ID auto generation

--- WebPages/assets/js/custom/custom.js
+++ WebPages/assets/js/custom/custom.js
@@ -1,3 +1,7 @@
+
+import { view_counter_http_api_Id} from '../../keys/view_counter_http_api.js';
+import { My_HTTP_API_Id} from '../../keys/My_HTTP_API.js';
+
 let form = document.getElementsByTagName("form")[0];
 if (form){
     form.addEventListener("submit", async (e) => {
@@ -8,7 +12,7 @@ if (form){
         email: theFormData.get('email'),
         message: theFormData.get('message')
         });
-        let ContactApiURL = `https://hbo8b661mc.execute-api.eu-west-2.amazonaws.com/prod/items`;
+        let ContactApiURL = `https://${My_HTTP_API_Id}.execute-api.eu-west-2.amazonaws.com/prod/items`;
         let ContactResponse = await fetch(ContactApiURL, {
           method: 'POST',
           headers: {
@@ -26,7 +30,7 @@ if (form){
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     try{
-      let ViewCountApiURL = `https://4dz632zab1.execute-api.eu-west-2.amazonaws.com/prod/items`;
+      let ViewCountApiURL = `https://${view_counter_http_api_Id}.execute-api.eu-west-2.amazonaws.com/prod/items`;
       let response = await fetch(ViewCountApiURL, {
         method: 'GET',
         headers: {
