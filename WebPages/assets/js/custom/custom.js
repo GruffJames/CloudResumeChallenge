db97f65d27e6abdb9ed7810a21067813ed44eb9c
Gruff James <gruffjameswork@gmail.com>
2024-06-15 20:50:22 +0100

    Fix issues with the form

--- WebPages/assets/js/custom/custom.js
+++ WebPages/assets/js/custom/custom.js
@@ -5,25 +5,32 @@ import { My_HTTP_API_Id} from '../../keys/My_HTTP_API.js';
 let form = document.getElementsByTagName("form")[0];
 if (form){
     form.addEventListener("submit", async (e) => {
-        e.preventDefault();
-        let theFormData = new FormData(form);
-        let MessageBody = JSON.stringify({
-        name: theFormData.get('name'),
-        email: theFormData.get('email'),
-        message: theFormData.get('message')
-        });
-        let ContactApiURL = `https://${My_HTTP_API_Id}.execute-api.eu-west-2.amazonaws.com/prod/items`;
-        let ContactResponse = await fetch(ContactApiURL, {
-          method: 'POST',
-          headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-              'Access-Control-Allow-Origin': '*'
-          },
-          body: MessageBody
-        })
-        window.location.href = '#ContactSent';
-        form.reset();
+
+        try {
+          e.preventDefault();
+          let theFormData = new FormData(form);
+          let MessageBody = JSON.stringify({
+          name: theFormData.get('name'),
+          email: theFormData.get('email'),
+          message: theFormData.get('message')
+          });
+          let ContactApiURL = `https://${My_HTTP_API_Id}.execute-api.eu-west-2.amazonaws.com/prod/items`;
+          let ContactResponse = await fetch(ContactApiURL, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: MessageBody
+          })
+          window.location.href = '#ContactSent';
+          form.reset();      
+        } catch (error) {
+          // const errorMessage = document.querySelector(".ContactIntroMessage");
+          // // window.location.href = '#ContactSentFailed';
+          // errorMessage.innerHTML = "Fill in all of the fields";
+        }
     });
 }
 
