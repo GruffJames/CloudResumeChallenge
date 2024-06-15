373ddd306ed0cbe0984dfa15814c94e63a5d6b3d
Gruff James <gruffjameswork@gmail.com>
2024-06-15 21:41:29 +0100

    why does this not work?????????

--- DockerServer/build_start_docker.ps1
+++ DockerServer/build_start_docker.ps1
@@ -1,2 +1,2 @@
 docker build -t webserver_image -f ./DockerServer/dockerfile . 
-docker run --name nginx_webserver -d -p 8080:80 webserver_image
+docker run --name nginx_webserver -d -p 8080:80 webserver_image -v ~/WebPages:/usr/share/nginx/html
