a8ddeaef1ea6bd8318259a344de107fafed496cd
Gruff James <gruffjameswork@gmail.com>
2024-12-20 22:58:40 +0000

    Fix issue with contact us field input type="text" being removed incorrectly

--- WebPages/src/App.js
+++ WebPages/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+function Contact() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h2 class="major">Contact</h2>
+        <form method="post" action="#">
+          <div class="fields">
+            <div class="field half">
+              <label for="name">Name</label>
+              <input type="text" name="name" id="name" />
+            </div>
+            <div class="field half">
+              <label for="email">Email</label>
+              <input type="email" name="email" id="email" />
+            </div>
+            <div class="field">
+              <label for="message">Message</label>
+              <textarea name="message" id="message" rows="1"></textarea>
+            </div>
+          </div>
+          <ul class="actions">
+            <li><input type="submit" value="Send Message" class="primary" /></li>
+            <li><input type="reset" value="Reset" /></li>
+          </ul>
+        </form>
+        <ul class="icons">
+          <li><a href="https://www.linkedin.com/in/gruff-james/" class="icon brands fa-linkedin"><span class="label">LinkedIn</span></a></li>
+          <li><a href="https://github.com/GruffJames" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+        </ul>
+      </div>
   );
 }
 
-export default App;
+export default Contact;
